07b970cc655fb11a43f12e8bc1b369b1e8f35df5
Maksym Syvash <maksym_syvash@outlook.com>
2024-12-06 14:51:13 +0200

    fixed main.py to expose connexion app

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -4,17 +4,15 @@ import connexion
 
 from openapi_server import encoder
 
+app = connexion.App(__name__, specification_dir='./openapi/')
+app.app.json_encoder = encoder.JSONEncoder
+
+app.add_api('openapi.yaml',
+            arguments={'title': 'Books Management API'},
+            pythonic_params=True)
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    
-    app.add_api('openapi.yaml',
-                arguments={'title': 'Books Management API'},
-                pythonic_params=True)
-
     app.run(port=8080)
-
 
 if __name__ == '__main__':
     main()
